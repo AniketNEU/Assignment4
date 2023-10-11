44ff24120e7742f995455c25a735ab47930a9b65
Aniket Tiwari <anikettiwari@Anikets-MacBook-Pro.local>
2023-10-11 00:15:22 -0400

    initial commit

---
+++ src/Model/Patient.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author anikettiwari
+ */
+public class Patient {
+    
+    private String firstName;
+    private String lastName;
+    private String ageText; 
+    private String email;
+    private String patientType;
+    private String gender;
+    private String message;
+    
+    public Patient(){
+        
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAgeText() {
+        return ageText;
+    }
+
+    public void setAgeText(String ageText) {
+        this.ageText = ageText;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPatientType() {
+        return patientType;
+    }
+
+    public void setPatientType(String patientType) {
+        this.patientType = patientType;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+}
